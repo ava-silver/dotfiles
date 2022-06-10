04cadb20f54f4ec435bc9a9756a3c2c2d956e597
Ava Silver <avapsilver@gmail.com>
2022-06-09 22:55:53 -0700

    Added automatic confirmation

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ export REPO_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/nul
 # Set up shell
 
 ## zsh
-sudo dnf install zsh
+sudo dnf install zsh -y
 
 ## oh-my-zsh
 export ZSH=$REPO_DIR/.oh-my-zsh
@@ -25,7 +25,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/z
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 ## useful packages
-sudo dnf install lsd thefuck zoxide fzf bat bpytop
+sudo dnf install lsd thefuck zoxide fzf bat bpytop -y
 
 ## link up everything else
 sudo bash $REPO_DIR/alias.sh
