936a2e54826c5806ef61618f9db17cac4b0cd2b7
Ava Silver <avapsilver@gmail.com>
2022-06-09 22:58:12 -0700

    fixed env setup

--- setup.sh
+++ setup.sh
@@ -28,4 +28,11 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/p
 sudo dnf install lsd thefuck zoxide fzf bat bpytop -y
 
 ## link up everything else
-sudo bash $REPO_DIR/alias.sh
+sudo echo "ZDOTDIR=$REPO_DIR" >> /etc/zshenv
+
+sed -i "1i \
+export XDG_CONFIG_HOME=$REPO_DIR\n\
+export XDG_DATA_HOME=$REPO_DIR/data\n\
+export XDG_STATE_HOME=$REPO_DIR/state\n\
+export XDG_CACHE_HOME=$REPO_DIR/cache\n\
+export ZSH=$REPO_DIR/.oh-my-zsh\n" $REPO_DIR/.zshrc
