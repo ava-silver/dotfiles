542ffd12589322aeb47fab23c22fdbb5710837d2
Ava Silver <avapsilver@gmail.com>
2022-12-10 18:07:05 -0500

    fixed up setup scripts

--- install_apps.sh
+++ install_apps.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/zsh
 
 # DNF Apps
-sudo dnf install alacritty snapd solaar gimp gnome-tweaks nextcloud-client python3 rust ImageMagick ffmpeg ranger vim -y
-sudo dnf module install nodejs:14 -y
+sudo dnf install firefox snapd solaar gimp gnome-tweaks nextcloud-client python3 rust cargo ImageMagick ffmpeg ranger sqlite3 vim -y --allowerasing
+[ ! -d "/home/ava/.nvm" ] && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 
 
 # Flatpak apps
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo flatpak install flathub com.spotify.Client
-sudo flatpak install flathub com.slack.Slack
-sudo flatpak install flathub com.bitwarden.desktop
-sudo flatpak install flathub com.github.tchx84.Flatseal
-sudo flatpak install flathub com.mattjakeman.ExtensionManager
-sudo flatpak install flathub com.microsoft.Edge
-sudo flatpak install flathub com.discordapp.Discord
+sudo flatpak install flathub com.spotify.Client -y
+sudo flatpak install flathub com.slack.Slack -y
+sudo flatpak install flathub com.bitwarden.desktop -y
+sudo flatpak install flathub com.github.tchx84.Flatseal -y
+sudo flatpak install flathub com.mattjakeman.ExtensionManager -y
+sudo flatpak install flathub com.microsoft.Edge -y
+sudo flatpak install flathub com.github.GradienceTeam.Gradience -y
 
 # Snap apps
+[ ! -L "/snap" ] && sudo ln -s /var/lib/snapd/snap /snap
 sudo snap install code --classic
 
 
@@ -24,5 +25,5 @@ sudo snap install code --classic
 # Minecraft
 # Tor
 # Monero GUI
-
+# Discord (betterdiscord)
 
