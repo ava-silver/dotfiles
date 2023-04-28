afa352dd0892659764fc3e4737ff8a55b99f6cce
Ava Silver <ava.silver@datadoghq.com>
2023-04-28 15:58:39 -0400

    reminder of other apps to install

--- setup.sh
+++ setup.sh
@@ -21,6 +21,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/p
 ## useful packagges 
 brew install lsd thefuck zoxide fzf bat bpytop rm-improved
 
+# unnatural scroll wheels, rectangle, yubikey manager, clipy will all need to be installed as well.
+
 # link zshrc, .p10k.zsh and zsh_aliases
 ln -s /Users/ava.silver/.dotfiles/.p10k.zsh /Users/ava.silver/.p10k.zsh
 ln -s /Users/ava.silver/.dotfiles/.zshrc /Users/ava.silver/.zshrc
